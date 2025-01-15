5d3b663e222670f1c0aab87900affeda4fa2901a
nilsdonatantonio <nils.donatantonio@heig-vd.ch>
2025-01-15 09:23:48 +0100

    Users and UsersController Created the classes based on the course examples Removed : - User email : no need of that field - User password : As security is not a main concern, no need of that   either, User will connect with id only

---
+++ lab06/src/main/java/ch/heigvd/dai/users/User.java
@@ -0,0 +1,12 @@
+package ch.heigvd.dai.users;
+
+public class User {
+
+  public Integer id;
+  public String firstName;
+  public String lastName;
+
+  public User() {
+    // Empty constructor for serialisation/deserialization
+  }
+}
